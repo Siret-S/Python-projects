2c02549e792de45ad373aac7114ea48dce5aafb3
unknown <siret.suigusaar@gmail.com>
2022-01-04 10:35:39 +0200

    Treasure island

---
+++ Treasure-island.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+left_right = input('You are at a cross road. Where to you want to go? Type "left" or "right"\n')
+if left_right == "left":
+  swim_wait = input('You have come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim acros\n')
+  if swim_wait == "wait":
+    door_color = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Witch color do you choose?\n")
+    if door_color == "red":
+      print("You opened a door to the room of fire and ar burned to death. Game over. ")
+    elif door_color == "blue":
+      print("You have been eaten by a beast. Game over. ")
+    elif door_color == "yellow":
+      print("You have found the treasure. YOU WIN!")
+    else :
+      print("Game over. ")
+  else:
+    print("You were attacked by trout. Game over. ")
+else:
+  print("You fell into a hole. Game Over.")
